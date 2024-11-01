8640fe8d03dbe880890fd14e927987deb8d1cf02
MadeSugi <imadesugih@gmail.com>
2024-11-01 23:27:36 +0700

    Delete SampleSecureWeb directory

--- SampleSecureWeb/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace SampleSecureWeb.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
